b4e8f2ca4d2970c221a7d6d56a6db1c2b39f67f5
James <jiraphatnam2204@gmail.com>
2026-02-28 16:25:56 +0700

    feat: add Postman environment and collection for Job Fair API

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,6 +7,8 @@ exports.protect = async (req, res, next) => {
 
     if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
+    } else if (req.cookies.token) {
+        token = req.cookies.token;
     }
 
     //Make sure token exists
